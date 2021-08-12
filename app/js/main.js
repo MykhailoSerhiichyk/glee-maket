26c1e1fe8714c6edde840be15641773aac84c5d0
StreetDream <mishkasaltovskiy@gmail.com>
2021-08-12 17:58:29 +0300

    added design section

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,23 @@
 $(function(){
 
+    var containerEl1 = document.querySelector('[data-ref="container-1"]');
+    var containerEl2 = document.querySelector('[data-ref="container-2"]');
 
     $('.top-slider__inner').slick({
         dots: true,
         arrows: false,
     });
 
+    
+    var config = {
+        controls: {
+            scope: 'local'
+        }
+    };
 
-    var mixer = mixitup('.products__items');
+
+    var mixer1 = mixitup(containerEl1, config);
+    var mixer2 = mixitup(containerEl2, config);
 
 
 
